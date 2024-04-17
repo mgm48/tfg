447be540c0e54c1274487aae23fb52ad6fb7b8a3
martagago48 <margago@ucm.es>
2024-04-17 22:30:29 +0200

    actualizado pruebas

--- pruebas.py
+++ pruebas.py
@@ -14,13 +14,14 @@ from src.vectordb import build_vectordb, load_faiss, load_chroma
 from streamlit_app.utils import perform
 from src.audio_player import AudioManager
 
+st.set_page_config(page_title="Conversación con Don Francisco de Arobe",layout="wide")
 
-
-st.set_page_config(page_title="Conversational Retrieval QA",layout="wide")
-
+#Engines para tts y stt
 audio_manager = AudioManager()
 engine = pyttsx3.init()
 r = sr.Recognizer()
+
+#Containers de steamlit
 c = st.container(height=410,border=False)
 c_extra = st.container(height=60,border=False)
 ee = c_extra.empty()
@@ -33,7 +34,7 @@ if "uploaded_filename" not in st.session_state:
 
 
 def init_chat_history():
-    #Initialise chat history.
+    #Inicializa el historial del chat
     clear_button = st.sidebar.button("Clear Conversation", key="clear")
     if clear_button or "chat_history" not in st.session_state:
         st.session_state["chat_history"] = list()
@@ -69,40 +70,13 @@ def config_tts(v=0, rate=200,vol=0.7,show_voices_info=False):
             print("\n")
             i+=1
 
-
-def css_changes():
-    script = """<div id = 'chat_outer'></div>"""
-    st.markdown(script, unsafe_allow_html=True)
-    with c:
-        script = """<div id = 'chat_inner'></div>"""
-        st.markdown(script, unsafe_allow_html=True)
-
-    chat_c_style = """<style>
-    div[data-testid='stVerticalBlock']:has(div#chat_inner):not(:has(div#chat_outer)) { };
-    </style>"""
-    st.markdown(chat_c_style, unsafe_allow_html=True) 
-
-    #form style
-    input_style = """<style>
-    div[data-testid="stForm"]{ position:fixed; bottom: 2%; border: 2px; padding: 10px; z-index: 10;}
-    </style>"""
-
-    st.markdown(input_style, unsafe_allow_html=True) 
-
-
-    button_style = """
-    <style>.element-container:has(#button-after) + div button {
-    position:fixed; bottom: 24px; border: 2px; padding: 10px; z-index: 10;
-    }</style>"""
-    st.markdown(button_style, unsafe_allow_html=True) 
-
 def grabar_callback():
     with sr.Microphone() as source:
             r.adjust_for_ambient_noise(source)
             with ee:
                 with st.spinner('Escuchando lo que dice...'):
                     audio = r.listen(source)
-                st.success('Done!')  
+                st.success('Hecho!')  
     try:
         res = r.recognize_google(audio, language='es-ES')
     except sr.UnknownValueError:
@@ -117,8 +91,6 @@ def borrar_callback():
 
 
 def doc_conv_qa():
-    #css_changes() 
-
     with st.sidebar:
         st.title("Conversational RAG with quantized LLM")
         image = Image.open('./assets/francisco.png')
@@ -128,18 +100,20 @@ def doc_conv_qa():
         )
         with st.expander("Configuración"):
             mode = st.radio(
-            "Modo",
+            "Modo de uso",
             ["text", "text + tts"],
-            index=1,
+            index=0,
             captions=["Solo responde usando texto.","Responde tanto con texto como audio."]
             )
+
 
     init_chat_history()
     st.sidebar.write("---")
     config_tts()
-    #c = st.container(height=410,border=False)
-    #c_extra = st.container(height=60,border=False)
-    for (question, answer) in st.session_state.chat_history:
+
+    # Desplegar historial del chat en container c
+    for (question, answer), source_documents in zip(
+        st.session_state.chat_history, st.session_state.source_documents):
         if question != "":
             with c:
                 with st.chat_message("user"):
@@ -147,48 +121,59 @@ def doc_conv_qa():
                 with st.chat_message("assistant"):
                     st.markdown(answer)
 
+            #with st.expander("Sources"):
+                #for row in source_documents:
+                    #st.write("**Page {}**".format(row.metadata["page"] + 1))
+                    #st.info(row.page_content)
+
     c1,c2 = st.columns([9,1])
     with c2:
-        #st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
-        grabar = st.button("Grabar",on_click=grabar_callback)
-        borrar = st.button("Borrar",on_click=borrar_callback)
-  
+        grabar = st.button("Grabar",help="Graba con un micrófono lo que quieras preguntarle a la IA",on_click=grabar_callback)
+        borrar = st.button("Borrar",help="Borra el contenido de la ventana de texto.",on_click=borrar_callback)
+
     with c1:
         input = st.form("form",clear_on_submit=False,border=True)
         with input:   
-            i1,i2 = st.columns([0.88,0.12])  
-            #if user_query := st.chat_input("Your query"):
+            i1,i2 = st.columns([0.85,0.15])  
             with i1:
-                #user_query = st.text_area("Tu pregunta", label_visibility="collapsed")
                 user_query = st.text_area("preg",f"",max_chars=1000, key = 'texto', label_visibility="collapsed")
 
             with i2:
-                submitted = st.form_submit_button("Preguntar")
+                submitted = st.form_submit_button("Pregunta", use_container_width=True)
                 if submitted:
                     if user_query == "":
                         ee.error("Por favor introduzca una pregunta.") 
-   
-                
+
+
     if user_query != "" and submitted:
         with c:
             with st.chat_message("user"):
                 st.markdown(user_query)
-            with st.chat_message("assistant"):
-                #if mode == "Texto":
-                #if mode == "Texto y audio":
-                question = user_query
-                answer = "Hola, soy Don Francisco de Arobe. Tengo cerca de sesenta años. Mi familia incluye a mi hijo el capitán don Pedro, de veintiún años, y a mi otro hijo don Domingo, de dieciocho. Nosotros somos descendientes de una mezcla de negro y indígena."
-                st.markdown(answer)
+            with ee:
+                with st.spinner('Obteniendo respuesta de la IA...'):
+                    response = "Hola Don José!"
 
-                st.session_state.chat_history.append((question, answer))
+                st.success('Hecho!') 
+                
+            ee.empty()    
+            with st.chat_message("assistant"):    
+                st.markdown(response)
 
-                if mode == "text + tts":
-                    filepath = "./src/tmp/res.wav"
-                    engine.save_to_file(answer,filepath)
-                    engine.runAndWait()
-                    audio_manager.play_audio(filepath, delete_file=True)
+        st.session_state.chat_history.append((user_query, response))
+        #st.session_state.source_documents.append(response["source_documents"])
 
-            
+        if mode == "text + tts":
+            filepath = "./src/tmp/res.wav"
+            engine.save_to_file(response,filepath)
+            engine.runAndWait()
+            audio_manager.play_audio(filepath, delete_file=False)
+
+            #Pestaña que te enseña de donde ha salido la respuesta
+            #with st.expander("Sources"):
+                #for row in response["source_documents"]:
+                    #st.write("**Page {}**".format(row.metadata["page"] + 1))
+                    #st.info(row.page_content)
+
 
 
 if __name__ == "__main__":
